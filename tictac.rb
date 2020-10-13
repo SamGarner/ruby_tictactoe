6de0ff907e4d89345ec8e789ef501a4c1b4db34b
SamGarner <samuel.w.garner@gmail.com>
2020-10-13 18:27:00 -0500

    update so that program exits if user chooses not to play again

--- tictac.rb
+++ tictac.rb
@@ -20,15 +20,17 @@ class Board
     puts "C: #{@board_hash[:C1]} | " + "#{@board_hash[:C2]} | " + @board_hash[:C3].to_s
   end
 
-  def play_again?
-    puts "Type 'y' if you would like to play again and any other key to exit :"
-    @play_again = gets.chomp.downcase
-    case @play_again
-    when 'y'
-      active_game = Game.new
-      active_game.play_game
-    end
-  end
+  # def play_again?
+  #   puts "Type 'y' if you would like to play again and any other key to exit :"
+  #   @play_again = gets.chomp.downcase
+  #   case @play_again
+  #   when 'y'
+  #     active_game = Game.new
+  #     active_game.play_game
+  #   # else
+  #   #   @current_game.game_over = 1
+  #   end
+  # end
 
   def check_for_win
     if @board_hash[:A1] != '-' &&
@@ -36,21 +38,22 @@ class Board
        [@board_hash[:A1], @board_hash[:B1], @board_hash[:C1]].uniq.length == 1 ||
        [@board_hash[:A1], @board_hash[:B2], @board_hash[:C3]].uniq.length == 1)
        puts "\n#{@board_hash[:A1]}'s win !!"
-       play_again?
+       # play_again?
     elsif @board_hash[:B2] != '-' &&
        ([@board_hash[:B2], @board_hash[:A2], @board_hash[:C2]].uniq.length == 1 ||
        [@board_hash[:B2], @board_hash[:A3], @board_hash[:C1]].uniq.length == 1 ||
        [@board_hash[:B2], @board_hash[:B1], @board_hash[:B3]].uniq.length == 1)
        puts "\n#{@board_hash[:B2]}'s win !!"
-       play_again?
+       # play_again?
     elsif @board_hash[:C3] != '-' &&
        ([@board_hash[:C3], @board_hash[:A3], @board_hash[:B3]].uniq.length == 1 ||
        [@board_hash[:C3], @board_hash[:C1], @board_hash[:C2]].uniq.length == 1)
        puts "\n#{@board_hash[:C3]}'s win !!"
-       play_again?
+       # play_again?
     elsif !@board_hash.has_value?('-')
       puts "Game over - it's a draw !"
-      play_again?
+      # play_again?
+    else 1
     end
   end
 end
@@ -106,13 +109,27 @@ class Game
     end
   end
 
+  def play_again?
+    puts "Type 'y' if you would like to play again and any other key to exit :"
+    @play_again = gets.chomp.downcase
+    case @play_again
+    when 'y'
+      active_game = Game.new
+      active_game.play_game
+    # else
+    #   @current_game.game_over = 1
+    end
+  end
+
   def play_game
     while @game_over == 0
       choose_space
       update_space
       @current_game.display_board
-      @current_game.check_for_win
+      # @current_game.check_for_win
+      @game_over = 1 if @current_game.check_for_win != 1
     end
+    play_again?
   end
 end
 
@@ -125,3 +142,5 @@ active_game.play_game
 # .to_s preferred to interpolation if only getting the hash value
 # to check that 3,4, + items all the same...can put them in an array together and then
 #   check that the length of uniq items is 1 - used for checking for winning combos
+# switch_active_player method 'direct assignment' -- see rubocop note: Check for 
+# if and case statements where each branch is used for assignment to the same variable when using the return of the condition can be used instead.
