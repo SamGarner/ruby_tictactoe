565c2b0d5a47198888b16260ad0ccaf458c4319d
SamGarner <samuel.w.garner@gmail.com>
2020-10-13 18:57:21 -0500

    update Player initialize to use %w array literal for @active_player sample

--- tictac.rb
+++ tictac.rb
@@ -63,7 +63,7 @@ class Player
   attr_reader :active_player # don't need accessor, just reader...
 
   def initialize
-    @active_player = ['X','O'].sample
+    @active_player = %w[X O].sample
     puts "\nTime for Tic Tac Toe !"
     puts "#{@active_player}'s will go first..."
   end
