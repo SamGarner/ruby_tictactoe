bb8d14cb1be712e6f1e68b035ff1edbd77df2859
SamGarner <samuel.w.garner@gmail.com>
2020-11-11 19:09:37 -0600

    four rspec tests

--- spec/tictac_spec.rb
+++ spec/tictac_spec.rb
@@ -13,11 +13,37 @@ describe Board do
       board.board_hash[:A1] = 'X'
       board.board_hash[:A2] = 'X'
       board.board_hash[:A3] = 'X'
-      expect do 
-        board.check_for_win()
-      end.to output("\n#{board.board_hash[:A1]}'s win !!\n").to_stdout#_from_any_process
-      # expect(board.check_for_win).to output("\nX's win !!").to_stdout
-      # expect(board.check_for_win).to eq("\nX's win !!")
+      expect { board.check_for_win }.to output("\n#{board.board_hash[:A1]}'s win !!\n").to_stdout#_from_any_process
+    end
+
+    it 'player wins when has diagnoals' do
+      board = Board.new
+      board.board_hash[:C1] = 'X'
+      board.board_hash[:B2] = 'X'
+      board.board_hash[:A3] = 'X'
+      expect { board.check_for_win }.to output("\n#{board.board_hash[:C1]}'s win !!\n").to_stdout
+    end
+
+    it 'player wins when has full third column' do
+      board = Board.new
+      board.board_hash[:C1] = 'O'
+      board.board_hash[:C2] = 'O'
+      board.board_hash[:C3] = 'O'
+      expect { board.check_for_win }.to output("\n#{board.board_hash[:C1]}'s win !!\n").to_stdout
+    end
+
+    it 'game ends in draw when no remaining playable spaces' do
+      board = Board.new
+      board.board_hash[:A1] = 'X'
+      board.board_hash[:A2] = 'X'
+      board.board_hash[:A3] = 'O'
+      board.board_hash[:B1] = 'O'
+      board.board_hash[:B2] = 'O'
+      board.board_hash[:B3] = 'X'
+      board.board_hash[:C1] = 'X'
+      board.board_hash[:C2] = 'O'
+      board.board_hash[:C3] = 'O'
+      expect { board.check_for_win }.to output("Game over - it's a draw !\n").to_stdout
     end
   end
 end
